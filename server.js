a72b825dced28834ae323db032551fe6b989f503
Ryan Roat <ryanroat@gmail.com>
2020-03-29 17:59:15 -0400

    middleware logs timestamp and url of request

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(logMW);
+
 app.get('/', (req, res) => {
-    res.send('Index');
+    res.send(`${new Date().toISOString()}: ${req.originalUrl}`);
 });
 
 app.get('/users', (req, res) => {
     res.send('Users Page');
 });
+
+app.get('/test', (req, res) => {
+    res.send('the test page');
+});
+
+function logMW(req, res, next) {
+    // console.log('MW...');
+    console.log(`${new Date().toISOString()}: ${req.originalUrl}`);
+    next();
+}
 
 app.listen(3000, () => console.log('server started'));
